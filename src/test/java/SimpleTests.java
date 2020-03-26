76aef1cc0bbbd3b0d498d9c3158c3eea904589f7
stfhrb <stfhrb@users.noreply.github.com>
2020-03-26 20:05:31 +0200

    modificarile mele

--- src/test/java/SimpleTests.java
+++ src/test/java/SimpleTests.java
@@ -63,7 +63,7 @@ public class SimpleTests {
 
         //2. navigam catre www.facebook.com
         // driver.get("https://www.facebook.com/"); // browserul ma va directa catre facebook.com
-        driver.navigate().to("https://www.facebook.com/"); // browserul ma va directa catre facebook.co
+        driver.navigate().to("https://www.facebook.com/"); // browserul ma va directa catre facebook.com
 
         //3. verificam ca suntem pe www.facebook.com
         //3.1. verificare logo
