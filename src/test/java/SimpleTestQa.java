839f936b3b0741aa2af10583a40293aa36ed3a87
Alex Fierascu <ioan.fierascu@gmail.com>
2020-03-30 19:28:49 +0300

    corectare tema

--- src/test/java/SimpleTestQa.java
+++ src/test/java/SimpleTestQa.java
@@ -7,38 +7,40 @@ import org.testng.annotations.Test;
 
 public class SimpleTestQa {
     @Test
-    public void testEmagPage(){
+    public void testEmagPage() {
         System.setProperty("webdriver.gecko.driver", "./src/main/resources/geckodriver.exe");
-        WebDriver driver= new FirefoxDriver();
+        WebDriver driver = new FirefoxDriver();
         driver.get("https://www.emag.ro/");
-        String titluRaspuns = "eMAG.ro - Libertate în fiecare zi";
+        String titluExpected = "eMAG.ro - Libertate în fiecare zi";
         String titlulActual = driver.getTitle();
-        Assert.assertEquals(titlulActual, titluRaspuns);
+        Assert.assertEquals(titlulActual, titluExpected);
         driver.quit();
     }
+
     @Test
-    public void testLogoEmag(){
-        System.setProperty("webdriver.gecko.driver","./src/main/resources/geckodriver.exe");
-        WebDriver driver= new FirefoxDriver();
+    public void testLogoEmag() {
+        System.setProperty("webdriver.gecko.driver", "./src/main/resources/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
         driver.navigate().to("https://www.emag.ro/");
-        WebElement logoEmag=driver.findElement(By.xpath("/html/body/div[4]/nav[1]/div/div/div[1]/a/img"));
+        WebElement logoEmag = driver.findElement(By.xpath("/html/body/div[4]/nav[1]/div/div/div[1]/a/img"));
         Assert.assertTrue(logoEmag.isDisplayed());
         driver.quit();
     }
+
     @Test
-    public void testMagazinLogin(){
-        System.setProperty("webdriver.gecko.driver","./src/main/resources/geckodriver.exe");
-        WebDriver driver= new FirefoxDriver();
+    public void testMagazinLogin() {
+        System.setProperty("webdriver.gecko.driver", "./src/main/resources/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
         driver.navigate().to("https://www.depurtat.ro/login");
-        WebElement inputEmail=driver.findElement(By.id("email"));
-        WebElement inputPass=driver.findElement(By.id("pass"));
-        WebElement magazinLoginButton=driver.findElement(By.xpath("/html/body/main/div/div/div/div[3]/div/div[1]/form/div[4]/div[2]/button")
+        Assert.assertEquals(driver.getTitle(), "Acces cont - dEpurtat");
+        WebElement inputEmail = driver.findElement(By.id("profile_email"));
+        WebElement inputPass = driver.findElement(By.id("profile_password"));
+        WebElement magazinLoginButton = driver.findElement(By.xpath("/html/body/main/div/div/div/div[3]/div/div[1]/form/div[4]/div[2]/button"));
         inputEmail.sendKeys("anna.nicullescu@gmail.com");
         inputPass.sendKeys("niculescu");
         magazinLoginButton.click();
-        Assert.assertEquals(driver.getTitle(), "Accesare cont");
-        WebElement xPathMesajEroare=driver.findElement(By.xpath("//*[@id=\"page_login_form\"]"));
-        Assert.assertTrue(xPathMesajEroare.isDisplayed());
+        WebElement mismatchPasswordMessage = driver.findElement(By.id("message_password_mismatch"));
+        Assert.assertTrue(mismatchPasswordMessage.isDisplayed());
         driver.quit();
     }
 
