45456df09a41fabd91e94ac30b27cc28a93544c0
Alex Fierascu <ioan.fierascu@gmail.com>
2020-03-30 20:51:59 +0300

    todo

--- src/test/java/SeleniumWaits.java
+++ src/test/java/SeleniumWaits.java
@@ -1,7 +1,5 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -11,12 +9,12 @@ public class SeleniumWaits extends BaseClass {
 
     @Test
     public void implicitWaitTest() {
-
-
-      String urlFainaAKAAurulAlbInTimesOfCoronaVirusEmag = "https://www.emag.ro/faina-neagra-de-grau-tip-1250-baneasa-1kg-45270311/pd/D0LY5BMBM/?X-Search-Id=d25a685ee0a7c590b0d6&X-Product-Id=6472108&X-Search-Page=1&X-Search-Position=2&X-Section=search&X-MB=0&X-Search-Action=view";
+        String urlFainaAKAAurulAlbInTimesOfCoronaVirusEmag = "https://www.emag.ro/faina-neagra-de-grau-tip-1250-baneasa-1kg-45270311/pd/D0LY5BMBM/?X-Search-Id=d25a685ee0a7c590b0d6&X-Product-Id=6472108&X-Search-Page=1&X-Search-Position=2&X-Section=search&X-MB=0&X-Search-Action=view";
         getDriver().get(urlFainaAKAAurulAlbInTimesOfCoronaVirusEmag);
-        WebDriverWait wait = new WebDriverWait(getDriver(),5);
+        WebDriverWait wait = new WebDriverWait(getDriver(), 5);
         wait.until(ExpectedConditions.titleIs("Faina neagra de grau tip 1250 Baneasa, 1kg - eMAG.ro"));
+        //todo
+        //documentatie implicit waits - avantaje/ dezavantaje
     }
 
 
@@ -27,9 +25,9 @@ public class SeleniumWaits extends BaseClass {
 
         WebElement numberOfReviews = getDriver().findElement(By.id("acrCustomerReviewText"));
         String actualNoOfReviews = numberOfReviews.getText();
-        System.out.println("numarul de reviews este " +actualNoOfReviews );
+        System.out.println("numarul de reviews este " + actualNoOfReviews);
         Thread.sleep(20000);
-        Assert.assertEquals(actualNoOfReviews,"72 ratings");
+        Assert.assertEquals(actualNoOfReviews, "72 ratings");
         //todo
         //documentatie explicit waits - avantaje/ dezavantaje
     }
